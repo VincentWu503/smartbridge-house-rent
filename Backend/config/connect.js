ef39f77ca11b3db6841b06a3675438969925b91a
VincentWu503 <vincenthendry06@gmail.com>
2026-09-23 16:40:05 +0700

    add cached connnection

--- Backend/config/connect.js
+++ Backend/config/connect.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  await mongoose
-    .connect(process.env.MONGO_DB)
-    .then(() => {
-      console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-      throw new Error(`Could not connect to MongoDB: ${err}`);
+// Use global to persist the connection across hot reloads in development
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      maxPoolSize: 5,
+      serverSelectionTimeoutMS: 30000,
+      bufferCommands: false,
     });
-};
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 module.exports = connectDB;
